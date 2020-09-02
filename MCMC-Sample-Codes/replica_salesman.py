1146d729e0dd3c9cd75af2cd1295fd9291bedaf8
tom01h <tom01h@gmail.com>
2020-09-02 15:07:52 +0900

    MCMC-Sample: salesman

---
+++ MCMC-Sample-Codes/replica_salesman.py
@@ -0,0 +1,98 @@
+nbeta=200
+niter=500000
+dbeta=0.5e0
+ncity=100
+ninit=1      # 0 -> read "100_cities.txt"; 1 -> continue; 2 -> random config.
+
+import sys
+import numpy as np
+import random
+import math
+import matplotlib.pyplot as plt
+import pickle
+
+def calc_distance(x, ordering, ibeta):
+    orderd = x[ordering[ibeta]]
+    r = orderd[1:] - orderd[:-1]
+    r = r * r
+    r = np.sum(r, axis=1)
+    r = np.sqrt(r)
+    return np.sum(r)
+
+minimum_distance = 100e0
+ordering = np.arange(0, ncity+1, 1)
+ordering = np.tile(ordering, (nbeta, 1))
+beta = np.arange(1, nbeta+1, 1, dtype = np.float32) * dbeta
+naccept = np.zeros(nbeta, dtype = np.int)
+distance_list = []
+
+if ninit == 0:
+    x = np.loadtxt('100_cities.txt', dtype = np.float32)
+    x = np.insert(x, ncity, x[0], axis=0)
+elif ninit == 1:
+    with open("salesman.pickle", "rb") as f:
+        x, ordering, minimum_ordering, minimum_distance, distance_list = pickle.load(f)
+elif ninit == 2:
+    x = np.random.rand(100, 2)
+    x = x.astype(np.float32)
+    x = np.insert(x, ncity, x[0], axis=0)
+
+distance = np.zeros(nbeta, dtype = np.float32)
+for ibeta in range(0, nbeta):
+    distance[ibeta] = calc_distance(x, ordering, ibeta)
+
+# Main loop #
+for iter in range(1, niter+1):
+    for ibeta in range(0, nbeta):
+        info_kl = 1
+        while info_kl == 1:
+            k = random.randrange(1, ncity)
+            l = random.randrange(1, ncity)
+            if k != l:
+                info_kl = 0
+        # Metropolis for each replica #
+        action_init = distance[ibeta] * beta[ibeta]
+        ordering[ibeta][k], ordering[ibeta][l] = ordering[ibeta][l], ordering[ibeta][k]
+        distance_fin = calc_distance(x, ordering, ibeta)
+        action_fin = distance_fin * beta[ibeta]
+        # Metropolis test #
+        metropolis = random.random()
+        if math.exp(action_init - action_fin) > metropolis:
+            distance[ibeta] = distance_fin
+            naccept[ibeta] = naccept[ibeta] + 1
+        else:
+            ordering[ibeta][k], ordering[ibeta][l] = ordering[ibeta][l], ordering[ibeta][k]
+    # Exchange replicas #
+    for ibeta in range(0, nbeta-1):
+        action_init  = distance[ibeta] * beta[ibeta]   + distance[ibeta+1] * beta[ibeta+1]
+        action_fin   = distance[ibeta] * beta[ibeta+1] + distance[ibeta+1] * beta[ibeta]
+        # Metropolis test #
+        metropolis = random.random()
+        if math.exp(action_init - action_fin) > metropolis:
+            ordering[ibeta], ordering[ibeta+1] = ordering[ibeta+1], ordering[ibeta]
+    # data output #
+    if iter % 500 == 0:
+        distance_200 = distance[199]
+        if distance_200 < minimum_distance:
+            minimum_distance = distance_200
+            minimum_ordering = ordering[nbeta-1]
+        distance_list = np.append(distance_list, minimum_distance)
+        print(iter, distance_200, minimum_distance)
+
+    # save point #
+    if iter % 50000 == 0 or iter == niter:
+        with open("salesman.pickle", "wb") as f:
+            pickle.dump((x, ordering, minimum_ordering, minimum_distance, distance_list), f)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        orderd = x[minimum_ordering].T
+        plt.plot(orderd[0], orderd[1], marker='+')
+        plt.axis([0, 1, 0, 1])
+        ax.set_aspect('equal', adjustable='box')
+        plt.savefig("salesman.png")
+        plt.clf()
+
+        plt.plot(distance_list, marker='+')
+        plt.savefig("distance.png")
+        plt.clf()
--- dot_files/.bashrc
+++ dot_files/.bashrc
@@ -1,9 +1,11 @@
-export RISCV=/opt/riscv
 export SYSTEMC=/usr/local/systemc-2.3
+export RISCV=/opt/riscv
 
-export PATH=~/bin:"$PATH":"$RISCV/bin"
+export PATH=~/bin:"$PATH":/opt/riscv/bin/
+export EDITOR=vi
 
 alias ls='ls -F --ignore={NTUSER*,ntuser*}'
 alias l='ls -l'
 alias emacs='/mnt/c/emacs-25.2/bin/runemacs.exe'
-#alias vivado='/mnt/c/Xilinx/Vivado/2016.4/bin/unwrapped/win64.o/vvgl.exe C:\\Xilinx\\Vivado\\2016.4\\bin\\vivado.bat'
+alias pull='env GIT_SSH_COMMAND="ssh -i /home/tom01h/.ssh/id_rsa -F /dev/null" git pull'
+alias push='env GIT_SSH_COMMAND="ssh -i /home/tom01h/.ssh/id_rsa -F /dev/null" git push'
