75fb6d4b07ac3bf68cfdf2ccf3caa616b46e2863
tom01h <tom01h@gmail.com>
2020-11-18 22:11:45 +0900

    Python_Verilog: Verilator 4.101

--- Python_Verilog/setup.py
+++ Python_Verilog/setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, Extension
+import glob
+
+filelist  = ['top.cpp', 'verilated.cpp', 'verilated_vcd_c.cpp']
+filelist += glob.glob("sim/*.cpp")
 setup(name='top',
         version='1.0',
-        ext_modules=[Extension('top', ['top.cpp', 'sim/Vadd__ALLcls.cpp', 'sim/Vadd__ALLsup.cpp', 'verilated.cpp', 'verilated_vcd_c.cpp'])]
+        ext_modules=[Extension('top', filelist,extra_compile_args=['-DTRACE'])]
 )
